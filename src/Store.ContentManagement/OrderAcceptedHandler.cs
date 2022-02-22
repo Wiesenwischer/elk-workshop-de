0dfa1883e61a1f20fd4ce12018cf20e7deabb92b
Marcus Dammann <m.dammann@ams-erp.com>
2022-02-22 08:49:25 +0100

    Introduce namespace Store.ContentManagement

--- src/Store.ContentManagement/OrderAcceptedHandler.cs
+++ src/Store.ContentManagement/OrderAcceptedHandler.cs
@@ -5,27 +5,30 @@ using NServiceBus.Logging;
 using Store.Messages.Events;
 using Store.Messages.RequestResponse;
 
-public class OrderAcceptedHandler :
-    IHandleMessages<OrderAccepted>
+namespace Store.ContentManagement
 {
-    static ILog log = LogManager.GetLogger<OrderAcceptedHandler>();
-
-    public Task Handle(OrderAccepted message, IMessageHandlerContext context)
+    public class OrderAcceptedHandler :
+        IHandleMessages<OrderAccepted>
     {
-        if (DebugFlagMutator.Debug)
+        static ILog log = LogManager.GetLogger<OrderAcceptedHandler>();
+
+        public Task Handle(OrderAccepted message, IMessageHandlerContext context)
         {
-            Debugger.Break();
+            if (DebugFlagMutator.Debug)
+            {
+                Debugger.Break();
+            }
+
+            log.Info($"Order # {message.OrderNumber} has been accepted, Let's provision the download -- Sending ProvisionDownloadRequest to the Store.Operations endpoint");
+
+            // send out a request (a event will be published when the response comes back)
+            var request = new ProvisionDownloadRequest
+            {
+                ClientId = message.ClientId,
+                OrderNumber = message.OrderNumber,
+                ProductIds = message.ProductIds
+            };
+            return context.Send(request);
         }
-
-        log.Info($"Order # {message.OrderNumber} has been accepted, Let's provision the download -- Sending ProvisionDownloadRequest to the Store.Operations endpoint");
-
-        // send out a request (a event will be published when the response comes back)
-        var request = new ProvisionDownloadRequest
-        {
-            ClientId = message.ClientId,
-            OrderNumber = message.OrderNumber,
-            ProductIds = message.ProductIds
-        };
-        return context.Send(request);
     }
 }
--- src/Store.ContentManagement/ProvisionDownloadResponseHandler.cs
+++ src/Store.ContentManagement/ProvisionDownloadResponseHandler.cs
@@ -6,43 +6,46 @@ using NServiceBus.Logging;
 using Store.Messages.Events;
 using Store.Messages.RequestResponse;
 
-public class ProvisionDownloadResponseHandler :
-    IHandleMessages<ProvisionDownloadResponse>
+namespace Store.ContentManagement
 {
-    static ILog log = LogManager.GetLogger<ProvisionDownloadResponseHandler>();
-
-    Dictionary<string, string> productIdToUrlMap = new Dictionary<string, string>
-        {
-            {"videos", "https://particular.net/videos-and-presentations"},
-            {"training", "https://particular.net/onsite-training"},
-            {"documentation", "https://docs.particular.net/"},
-            {"customers", "https://particular.net/customers"},
-            {"platform", "https://particular.net/service-platform"},
-        };
-
-    public Task Handle(ProvisionDownloadResponse message, IMessageHandlerContext context)
+    public class ProvisionDownloadResponseHandler :
+        IHandleMessages<ProvisionDownloadResponse>
     {
-        if (DebugFlagMutator.Debug)
-        {
-            Debugger.Break();
-        }
+        static ILog log = LogManager.GetLogger<ProvisionDownloadResponseHandler>();
 
-        log.Info($"Download for Order # {message.OrderNumber} has been provisioned, Publishing Download ready event");
-
-        log.Info($"Downloads for Order #{message.OrderNumber} is ready, publishing it.");
-        var downloadIsReady = new DownloadIsReady
+        Dictionary<string, string> productIdToUrlMap = new Dictionary<string, string>
         {
-            OrderNumber = message.OrderNumber,
-            ClientId = message.ClientId,
-            ProductUrls = new Dictionary<string, string>()
+            { "videos", "https://particular.net/videos-and-presentations" },
+            { "training", "https://particular.net/onsite-training" },
+            { "documentation", "https://docs.particular.net/" },
+            { "customers", "https://particular.net/customers" },
+            { "platform", "https://particular.net/service-platform" },
         };
 
-        foreach (var productId in message.ProductIds)
+        public Task Handle(ProvisionDownloadResponse message, IMessageHandlerContext context)
         {
-            downloadIsReady.ProductUrls.Add(productId, productIdToUrlMap[productId]);
+            if (DebugFlagMutator.Debug)
+            {
+                Debugger.Break();
+            }
+
+            log.Info($"Download for Order # {message.OrderNumber} has been provisioned, Publishing Download ready event");
+
+            log.Info($"Downloads for Order #{message.OrderNumber} is ready, publishing it.");
+            var downloadIsReady = new DownloadIsReady
+            {
+                OrderNumber = message.OrderNumber,
+                ClientId = message.ClientId,
+                ProductUrls = new Dictionary<string, string>()
+            };
+
+            foreach (string productId in message.ProductIds)
+            {
+                downloadIsReady.ProductUrls.Add(productId, productIdToUrlMap[productId]);
+            }
+
+            return context.Publish(downloadIsReady);
+
         }
-        return context.Publish(downloadIsReady);
-
     }
-
 }
