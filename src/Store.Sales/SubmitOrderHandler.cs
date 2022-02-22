ba6b47cbb86f050e67a5291874b81bdc3751d36e
Marcus Dammann <m.dammann@ams-erp.com>
2022-02-22 10:38:37 +0100

    Structured logging in SubmitOrderHandler

--- src/Store.Sales/SubmitOrderHandler.cs
+++ src/Store.Sales/SubmitOrderHandler.cs
@@ -1,16 +1,23 @@
 ﻿using System.Diagnostics;
 using System.Threading.Tasks;
 using NServiceBus;
-using NServiceBus.Logging;
 using Store.Messages.Commands;
 using Store.Messages.Events;
+using Microsoft.Extensions.Logging;
 
 namespace Store.Sales
 {
-    public class SubmitOrderHandler :
+    using System;
+
+    internal class SubmitOrderHandler :
         IHandleMessages<SubmitOrder>
     {
-        static ILog log = LogManager.GetLogger<SubmitOrderHandler>();
+        readonly ILogger<SubmitOrderHandler> log;
+
+        public SubmitOrderHandler(ILogger<SubmitOrderHandler> log)
+        {
+            this.log = log ?? throw new ArgumentNullException(nameof(log));
+        }
 
         public Task Handle(SubmitOrder message, IMessageHandlerContext context)
         {
@@ -19,11 +26,12 @@ namespace Store.Sales
                 Debugger.Break();
             }
 
-            log.Info($"Received an order #{message.OrderNumber} for [{string.Join(", ", message.ProductIds)}] products(s).");
+            string products = string.Join(", ", message.ProductIds);
+            log.LogInformation("Received an Order #{OrderNumber} for [{ProductIds}] product(s).", message.OrderNumber, products);
 
-            log.Info("The credit card values will be encrypted when looking at the messages in the queues");
-            log.Info($"CreditCard Number is {message.CreditCardNumber}");
-            log.Info($"CreditCard Expiration Date is {message.ExpirationDate}");
+            log.LogInformation("The credit card values will be encrypted when looking at the messages in the queues");
+            log.LogInformation("CreditCard Number is {CreditCardNumber}", message.CreditCardNumber);
+            log.LogInformation("CreditCard Expiration Date is {ExpirationDate}", message.ExpirationDate);
 
             // tell the client the order was received
             var orderPlaced = new OrderPlaced
@@ -32,6 +40,7 @@ namespace Store.Sales
                 OrderNumber = message.OrderNumber,
                 ProductIds = message.ProductIds
             };
+            log.LogTrace("Publishing: {@Event}", orderPlaced);
             return context.Publish(orderPlaced);
         }
     }
